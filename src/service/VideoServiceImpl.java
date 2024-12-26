4a7d08b082feeebf0a4eb27859bb2ad6ef7db4d9
gsmzupper <gabriel.merconi@zup.com.br>
2024-12-26 09:46:12 -0300

    feat(VideoServiceImpl): refatora classe VideoServiceImpl com validações e melhorias

--- src/service/VideoServiceImpl.java
+++ src/service/VideoServiceImpl.java
@@ -3,22 +3,30 @@ package service;
 import model.Video;
 import repository.VideoRepository;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class VideoServiceImpl implements VideoService {
     private final VideoRepository repository;
 
     public VideoServiceImpl(VideoRepository repository) {
+        if (repository == null) {
+            throw new IllegalArgumentException("O repositório não pode ser nulo.");
+        }
         this.repository = repository;
     }
 
     @Override
     public void addVideo(Video video) {
+        if (video == null) {
+            throw new IllegalArgumentException("O vídeo não pode ser nulo.");
+        }
         repository.save(video);
     }
 
     @Override
     public List<Video> listVideos() {
-        return repository.findAll();
+        return repository.findAll().stream().sorted(Comparator.comparing(Video::getTitulo)).collect(Collectors.toUnmodifiableList());
     }
 }
